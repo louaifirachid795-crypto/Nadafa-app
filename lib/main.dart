bae2a7d0fba7aff9e1f794d1bb79b17015fa16d8
louaifirachid795-crypto <louaifirachid795@gmail.com>
2026-09-15 18:21:44 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 void main() {
   runApp(const NadafaApp());
 }
-
 class NadafaApp extends StatelessWidget {
   const NadafaApp({super.key});
 
