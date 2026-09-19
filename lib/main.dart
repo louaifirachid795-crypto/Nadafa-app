5fd540a2a404d4c37a46c63c930590b5e35b1702
rachid lwaifi <louaifirachid795@gmail.com>
2026-09-19 01:13:59 +0100

    Add markers to GoogleMap in main.dart
    Added markers to the GoogleMap widget in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -149,21 +149,17 @@ class _CitizenPageState extends State<CitizenPage> {
       body: Column(
         children: [
 
-          SizedBox(
-  height: 350,
-  width: double.infinity,
-  child: GoogleMap(
-    initialCameraPosition: const CameraPosition(
-      target: LatLng(36.7538, 3.0588),
-      zoom: 14,
-    ),
-    zoomControlsEnabled: true,
-    myLocationButtonEnabled: true,
-  ),
-),
-
+                  SizedBox(
+            height: 350,
+            width: double.infinity,
+            child: GoogleMap(
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(36.7538, 3.0588),
+                zoom: 14,
+              ),
               markers: markers,
-
+              zoomControlsEnabled: true,
+              myLocationButtonEnabled: true,
               onTap: (LatLng position) {
                 setState(() {
                   selectedHome = position;
